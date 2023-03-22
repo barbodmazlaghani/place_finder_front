55cb42416ef0b876efe813fc184bcf2121d6b5c5
barbodmazlaghani <57978208+barbodmazlaghani@users.noreply.github.com>
2023-03-22 15:15:22 +0430

    UserList and UserItem Added ( unstyled )

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UserItem from "./UserItem";
+
+const UsersList = (props) => {
+    if (props.items.length === 0) {
+        return (
+            <div className={"center"}>
+                <h2>No Users found!</h2>
+            </div>
+        )
+    }
+    return <ul>
+        {props.items.map(user =>
+            <UserItem key={user.id} id={user.id} name={user.name} image={user.image} placeCount={user.places}/>)}
+    </ul>
+}
+export default UsersList;
